791d43e97b6f708a5f09799a2eceda7ab028fe7a
Tom Myers <tmyers1@yahoo.com>
2026-05-13 21:15:03 -0400

    Added unused import

--- src/test/java/com/djt/jukeanator_engine/domain/songplayer/service/SongPlayerServiceTest.java
+++ src/test/java/com/djt/jukeanator_engine/domain/songplayer/service/SongPlayerServiceTest.java
@@ -1,6 +1,5 @@
 package com.djt.jukeanator_engine.domain.songplayer.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
