06b0c10abc5952cb385a08578155d763cbc1ebad
Tom Myers <tmyers1@yahoo.com>
2026-05-23 07:19:12 -0400

    Formatted RootFolderEntity

--- src/main/java/com/djt/jukeanator_engine/domain/songlibrary/model/RootFolderEntity.java
+++ src/main/java/com/djt/jukeanator_engine/domain/songlibrary/model/RootFolderEntity.java
@@ -13,14 +13,14 @@ import com.djt.jukeanator_engine.domain.common.exception.EntityDoesNotExistExcep
 
 public class RootFolderEntity extends FolderEntity {
   private static final long serialVersionUID = 1L;
-  
+
   private String rootPrefix;
 
   private transient Map<Integer, GenreFolderEntity> genres;
   private transient Map<Integer, ArtistFolderEntity> artists;
   private transient Map<Integer, AlbumFolderEntity> albums;
   private transient Map<String, SongFileEntity> songs;
-  
+
   public RootFolderEntity() {}
 
   /**
@@ -87,14 +87,14 @@ public class RootFolderEntity extends FolderEntity {
   }
 
   public List<AlbumFolderEntity> getAllAlbums() {
-    
+
     Set<AlbumFolderEntity> allAlbums = new TreeSet<>();
-    
+
     for (FolderEntity childFolder : getChildFolders()) {
 
       if (childFolder instanceof AlbumFolderEntity) {
 
-        allAlbums.add((AlbumFolderEntity)childFolder);
+        allAlbums.add((AlbumFolderEntity) childFolder);
 
       } else {
 
@@ -102,9 +102,9 @@ public class RootFolderEntity extends FolderEntity {
 
       }
     }
-    
+
     ArrayList<AlbumFolderEntity> list = new ArrayList<>();
-    list.addAll(allAlbums);    
+    list.addAll(allAlbums);
     return list;
   }
 
@@ -112,7 +112,7 @@ public class RootFolderEntity extends FolderEntity {
   public FolderEntity getParentFolder() {
     throw new IllegalStateException("getParentFolder() cannot be called on the Root");
   }
-  
+
   @Override
   public String getNaturalIdentity() {
 
@@ -137,80 +137,82 @@ public class RootFolderEntity extends FolderEntity {
   public Collection<AlbumFolderEntity> getAlbums() {
     return albums.values();
   }
-  
+
   public Collection<SongFileEntity> getSongs() {
     return songs.values();
   }
-  
+
   public void initialize() {
-    
+
     this.genres = new HashMap<>();
     this.artists = new HashMap<>();
     this.albums = new HashMap<>();
     this.songs = new HashMap<>();
-    
+
     for (AlbumFolderEntity album : getAllAlbums()) {
-                
+
       GenreFolderEntity genre = album.getParentGenre();
-      Integer genreId = genre.getPersistentIdentity();      
+      Integer genreId = genre.getPersistentIdentity();
       if (!this.genres.containsKey(genreId)) {
         this.genres.put(genreId, genre);
       }
-      
+
       ArtistFolderEntity artist = album.getParentArtist();
       Integer artistId = artist.getPersistentIdentity();
       if (!this.artists.containsKey(artistId)) {
-          this.artists.put(artistId, artist);
+        this.artists.put(artistId, artist);
       }
-      
+
       Integer albumId = album.getPersistentIdentity();
       if (!this.albums.containsKey(albumId)) {
         this.albums.put(albumId, album);
       }
-      
-      for (SongFileEntity song: album.getChildSongs()) {
+
+      for (SongFileEntity song : album.getChildSongs()) {
         String songKey = albumId.toString() + song.getPersistentIdentity().toString();
         if (!this.songs.containsKey(songKey)) {
           this.songs.put(songKey, song);
-        }        
+        }
       }
-    }    
+    }
   }
 
   public GenreFolderEntity getGenreById(Integer id) throws EntityDoesNotExistException {
-    
+
     GenreFolderEntity entity = genres.get(id);
     if (entity != null) {
       return entity;
-    }   
+    }
     throw new EntityDoesNotExistException("Genre with id: [" + id + "] not found.");
-  }  
+  }
 
   public ArtistFolderEntity getArtistById(Integer id) throws EntityDoesNotExistException {
-    
+
     ArtistFolderEntity entity = artists.get(id);
     if (entity != null) {
       return entity;
-    }   
+    }
     throw new EntityDoesNotExistException("Artist with id: [" + id + "] not found.");
-  }  
-  
+  }
+
   public AlbumFolderEntity getAlbumById(Integer id) throws EntityDoesNotExistException {
-    
+
     AlbumFolderEntity entity = albums.get(id);
     if (entity != null) {
       return entity;
-    }   
+    }
     throw new EntityDoesNotExistException("Album with id: [" + id + "] not found.");
   }
-  
-  public SongFileEntity getSongById(Integer albumId, Integer songId) throws EntityDoesNotExistException {
-    
+
+  public SongFileEntity getSongById(Integer albumId, Integer songId)
+      throws EntityDoesNotExistException {
+
     String songKey = albumId.toString() + songId.toString();
     SongFileEntity entity = songs.get(songKey);
     if (entity != null) {
       return entity;
-    }   
-    throw new EntityDoesNotExistException("Song with songId: [" + songId + "] and albumId: [" + albumId + "] not found.");
-  } 
+    }
+    throw new EntityDoesNotExistException(
+        "Song with songId: [" + songId + "] and albumId: [" + albumId + "] not found.");
+  }
 }
